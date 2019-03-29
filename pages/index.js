9c8d2d436ccc9861fa109f9bf18be3d2e99016b2
iammarkps <markkung08@gmail.com>
2019-03-29 10:00:16 +0700

    Refactor

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,12 @@ import React from 'react'
 
 import { Title } from '../components/Title'
 import { Layout } from '../components/Layout'
-import { NavBar } from '../components/NavBar'
 import { Projects } from '../components/Projects'
 import { About } from '../components/About'
 import { Footer } from '../components/Footer'
 
-import { DesktopOnly } from '../components/Responsive'
-
-const menu = [
-  { text: 'Home', href: '/' },
-  { text: 'Project', href: '/' },
-  { text: 'About', href: '/' }
-]
-
-const more = [{ text: 'Github', href: '/' }]
-
 export default () => (
   <Layout>
-    <DesktopOnly>
-      <NavBar title="TRIAM UDOM" menu={menu} more={more} />
-    </DesktopOnly>
     <Title />
     <Projects />
     <About />
