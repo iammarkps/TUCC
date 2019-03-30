f1facd0c4283dc62a9ce8dc54e436ab9f5dffec4
iammarkps <markkung08@gmail.com>
2019-03-30 20:04:42 +0700

    Add social meta

---
+++ components/SocialMeta.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const SocialMeta = () => {
+  return (
+    <React.Fragment>
+      <meta property="og:title" content="" />
+      <meta property="og:description" content="" />
+      <meta property="og:image" content="" />
+      <meta property="og:url" content="https://triamudom.cc" />
+      <meta name="twitter:card" content="summary_large_image" />
+    </React.Fragment>
+  )
+}
